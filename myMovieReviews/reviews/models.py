ccc2762e151b815be0417311fd9b6165eea8916b
shash0423 <shash042319@naver.com>
2024-01-12 20:50:22 +0900

    update에서 choices 기능 추가

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -4,7 +4,14 @@ class Review(models.Model):
     title = models.CharField(max_length=32)
     director = models.CharField(max_length=32)
     mainActor = models.CharField(max_length=32)
-    genre = models.CharField(max_length=32)
+    GENRE_CHOICES = [
+        ('action', '액션'),
+        ('comedy', '코미디'),
+        ('drama', '드라마'),
+        ('sf', 'SF'),
+        ('horror', '공포'),
+    ]
+    genre = models.CharField(max_length=10, choices=GENRE_CHOICES)
     horoscope = models.IntegerField()
     runningTime = models.IntegerField()
     reviewContent = models.TextField()
