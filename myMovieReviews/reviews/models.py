de4d58e584a9511678f4f6e9df81b3ff201f9c2c
shash0423 <shash042319@naver.com>
2024-01-12 23:19:19 +0900

    css 수정

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -5,13 +5,13 @@ class Review(models.Model):
     director = models.CharField(max_length=32)
     mainActor = models.CharField(max_length=32)
     GENRE_CHOICES = [
-        ('action', '액션'),
-        ('comedy', '코미디'),
-        ('drama', '드라마'),
-        ('sf', 'SF'),
-        ('horror', '공포'),
+        ("action", "액션"),
+        ("comedy", "코미디"),
+        ("drama", "드라마"),
+        ("sf", "SF"),
+        ("horro", "공포"),
     ]
-    genre = models.CharField(max_length=10, choices=GENRE_CHOICES)
+    genre = models.CharField(max_length=10, choices=GENRE_CHOICES, default="action")
     horoscope = models.IntegerField()
     runningTime = models.IntegerField()
     reviewContent = models.TextField()
